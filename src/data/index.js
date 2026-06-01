66efbf8ebc4cfd9d6c05b4906c40ae13fe5fd26a
Peugcam <pauloeugeniocampos18k@gmail.com>
2026-06-01 20:00:09 -0300

    feat: dados reais das tres competicoes (jun/2026)

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import libertadores from "./libertadores.json";
+import copaBrasil from "./copa-brasil.json";
+import mundial from "./mundial.json";
+
+export const COMPETICOES = { libertadores, "copa-brasil": copaBrasil, mundial };
+export const LISTA = [mundial, libertadores, copaBrasil];
